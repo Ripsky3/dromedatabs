d3f729574f71cd3239afa6a561358db8c85c83ba
titus <titushrangthawng@gmail.com>
2023-11-16 01:11:35 -0500

    Deciding to not test anymore

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
-const app = require("./app.js");
+const express = require("express");
+const path = require("path");
+var bodyParser = require('body-parser')
+require("./db/mongoose");
+
+const { userRouter } = require("./routes/user");
+const { itemRouter } = require("./routes/item");
+const { messageRouter } = require("./routes/message");
+const { ratingRouter } = require("./routes/rating");
+const { buyRouter } = require("./routes/buy");
+
+const app = express();
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+app.set("view engine", "hbs");
+app.use(express.static(publicDirectoryPath));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Use routers
+app.use(userRouter);
+app.use(itemRouter);
+app.use(messageRouter);
+app.use(ratingRouter);
+app.use(buyRouter);
 
 const port = process.env.PORT || 3000; 
 
