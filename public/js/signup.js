9359ba4774cb7c3c0595f869933e9396c73d610f
titus <titushrangthawng@gmail.com>
2023-12-24 21:09:08 -0500

    Got rid of address feature

--- public/js/signup.js
+++ public/js/signup.js
@@ -16,22 +16,7 @@ userFormButton.addEventListener("click", (e) => {
 })
 
 async function createUser() {
-    if (!navigator.geolocation) {
-        throw new Error({error: "Geolocation is not supported by your browser"});
-    }
-    let latitude;
-    let longitude;
-    const geo = await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            latitude = position.coords.latitude;
-            longitude = position.coords.longitude;
-            resolve();
-            if (!latitude || !longitude) {
-                reject();
-            }
-        });  
-    })
-    await geo;
+    
     const response = await fetch("/createuser", {
         method: 'POST',
         mode: 'cors',
@@ -39,9 +24,7 @@ async function createUser() {
         body: JSON.stringify({
             username: usernameInput.value,
             email: emailInput.value,
-            password: passwordInput.value,
-            latitude: latitude.toString(),
-            longitude: longitude.toString()
+            password: passwordInput.value
         })
     }).then(res => res.json());
     return response
